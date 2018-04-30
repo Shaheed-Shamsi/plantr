5564aabca177296fa3b6a1c6e109230c7ff05c16
Heather Allyse <heather.haskin@outlook.com>
2018-04-30 17:07:33 -0500

    heather drove

--- models.js
+++ models.js
@@ -2,28 +2,36 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/plantr');
 
 const Gardener = db.define('gardener', {
-    name: Sequelize.STRING,
-    age: Sequelize.INTEGER,
+   name: Sequelize.STRING,
+   age: Sequelize.INTEGER,
 })
 const Plot = db.define('plot', {
-  size: Sequelize.INTEGER,
-  shaded: Sequelize.BOOLEAN,
+ size: Sequelize.INTEGER,
+ shaded: Sequelize.BOOLEAN,
 })
 
 const Vegetable = db.define('vegetable', {
-  name: Sequelize.STRING,
-  color: Sequelize.STRING,
-  planted_on: Sequelize.DATE,
+ name: Sequelize.STRING,
+ color: Sequelize.STRING,
+ planted_on: {
+   type: Sequelize.DATE,
+   defaultValue: Sequelize.NOW
+ }
 })
 
 
-Plot.hasOne(Gardener)
-Gardener.belongsTo(Plot);
+Plot.belongsTo(Gardener)
+Gardener.hasOne(Plot);
 
 Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'});
 Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'});
 Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'});
 
 
-module.exports = db;
+module.exports = {
+ db,
+ Plot,
+ Vegetable,
+ Gardener
+};
 
--- seed.js
+++ seed.js
@@ -1,14 +1,43 @@
-const db = require('./models');
+const { db } = require('./models');
+const { Gardener } = require('./models');
+const { Plot } = require('./models');
+const { Vegetable } = require('./models');
+
+let SeedFunction = function() {
+   return
+}
+
+
 db.sync({force: true})
 .then(()=>{
-  console.log('the Database has been synced')
+ console.log('the Database has been synced')
+})
+.then(() => Vegetable.create({
+  name: 'Carrot',
+  color: 'Orange',
+}))
+.then(carrot =>
+ Gardener.create({
+  name: "Herbert",
+  age: 50,
+  favoriteVegetableId: carrot.id,
+ })
+)
+.then(gardener =>
+  Plot.create({
+    size: 2,
+    shaded: false,
+    gardenerId: gardener.id,
+  })
+)
+.catch((e) => {
+ console.log('Disaster! Something went wrong!')
+ console.error(e);
+})
+.finally(() => {
   db.close();
 })
-.catch((e) => {
-  console.log('Disaster! Something went wrong!')
-  console.error(e);
-  db.close()
-})
 
-//returns a promise with resolves if the sync is successful and rejects with anerror if omething is wrong
 
+
+//returns a promise with resolves if the sync is successful and rejects with anerror if omething is wron
